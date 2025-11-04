c7e966de92743221d37d6c37f902b4cf2e938c84
Niukkis2 <basanaama@gmail.com>
2025-11-04 20:23:17 +0200

    day03 part1

---
+++ Day03/day03.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('C:/Users/nm/Programming/AoC2024/Day03/day03input.txt', 'utf-8');
+console.log('P1: ' + p1(input));
+
+function p1(input) {
+    const instructions = [];
+    const pattern = /mul\(\d+?,\d+?\)/gm;
+    input.split("\n").forEach(part => {
+        const found = part.match(pattern);
+        if (found) instructions.push(...found);
+    });
+    let sum = 0;
+    instructions.forEach(instruction => {
+        let pair = instruction.slice(4, instruction.length - 1).split(",");
+        let multiplied = parseInt(pair[0]) * parseInt(pair[1]);
+        sum += multiplied;
+    });
+    return sum;
+}
