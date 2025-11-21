a47ece28b2b869eadd1421f30692fce01fee7f88
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-11-21 10:52:12 +0200

    day5 p2 in python

--- day05/day05.py
+++ day05/day05.py
@@ -3,17 +3,21 @@ def parseInput():
     lines = f.readlines()
     splitter = lines.index("\n")
     rules = lines[0:splitter]
-    pages = lines[splitter + 1 : len(lines)]
+    pages = lines[splitter + 1:len(lines)]
     rules = [rule.strip() for rule in rules]
     pages = [page.strip("\n") for page in pages]
     pages = [list(map(int, page.split(","))) for page in pages]
     return [rules, pages]
 
+
 def p1(input):
     rules = input[0]
     pages = input[1]
-    middleSum = 0
+    middleSumCorrect = 0
+    middleSumIncorrect = 0
     correct = []
+    incorrect = []
+    # p1
     for i in range(len(pages)):
         inOrder = True
         for j in range(len(rules)):
@@ -24,9 +28,27 @@ def p1(input):
                     inOrder = False
         if inOrder:
             correct.append(pages[i])
+        else:
+            incorrect.append(pages[i])
     for numList in correct:
-        middleSum += numList[len(numList) // 2]
-    return middleSum
+        middleSumCorrect += numList[len(numList) // 2]
+    # p2
+    for i in range(len(incorrect)):
+        swapped = True
+        while swapped:
+            swapped = False
+            for j in range(len(rules)):
+                leftnum = int(rules[j].split("|")[0])
+                rightnum = int(rules[j].split("|")[1])
+                if leftnum in incorrect[i] and rightnum in incorrect[i]:
+                    leftIndex = incorrect[i].index(leftnum)
+                    rightIndex = incorrect[i].index(rightnum)
+                    if leftIndex > rightIndex:
+                        incorrect[i][leftIndex], incorrect[i][rightIndex] = incorrect[i][rightIndex], incorrect[i][leftIndex]
+                        swapped = True
+    for numList in incorrect:
+        middleSumIncorrect += numList[len(numList) // 2]
+    print("P1: " + str(middleSumCorrect) + "\nP2: " + str(middleSumIncorrect))
 
-print(p1(parseInput()))
 
+p1(parseInput())
